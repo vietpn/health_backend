cd09b90f413fe892a97fed5fe9cb5db8d2e7ef4d
Pham Viet <vietpn@vnptepay.com>
2019-05-29 16:09:59 +0700

    update

--- resources/views/backend/profiles/fields.blade.php
+++ resources/views/backend/profiles/fields.blade.php
@@ -25,7 +25,18 @@
 <!-- Birthday Field -->
 <div class="form-group">
     {!! Form::label('birthday', 'Birthday:') !!}
-    {!! Form::date('birthday', null, ['class' => 'form-control']) !!}
+    <div class="input-group">
+        <?php
+        if ( isset($profile) && !empty($profile->birthday)) {
+            echo Form::text('birthday', Request::old('birthday', $profile->birthday),['class'=>'form-control', 'id'=>'birthday']);
+        }else {
+            echo Form::text('birthday', Request::old('birthday', ''),['class'=>'form-control', 'id'=>'birthday']);
+        }
+        ?>
+        <label class="input-group-addon btn" for="date">
+            <span class="fa fa-calendar open-datetimepicker"></span>
+        </label>
+    </div>
 </div>
 
 <!-- Password Field -->
@@ -39,3 +50,14 @@
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
     <a href="{!! route('backend.profiles.index') !!}" class="btn btn-default">Cancel</a>
 </div>
+
+
+<!--datepicker-->
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<!--end datepicker-->
+<script type="text/javascript">
+    $('#birthday').datepicker({
+        dateFormat: 'yy-mm-dd',
+    });
+</script>
