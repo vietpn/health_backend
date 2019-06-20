552e5fbe75566c1c360cf53f400a213681ed2eeb
Pham Viet <vietpn@vnptepay.com>
2019-06-20 14:22:42 +0700

    update

--- resources/views/backend/products/fields.blade.php
+++ resources/views/backend/products/fields.blade.php
@@ -10,7 +10,7 @@
     {!! Form::file('image', ['class' => 'form-control', 'accept' => 'image/*']) !!}
     <?php
     if ( isset($product) && !empty($product->img_path)) {
-        echo Html::image(\App\Models\BaseModel::getImage( $product->img_path) , '',['style' => 'width:80px; height:80px']);
+        echo Html::image($product->img_path, '',['style' => 'width:80px; height:80px']);
         echo "<br/>";
     } else {
         echo '<img class="thumbnail" src="http://placehold.it/100x100" alt="" width="100" height="100" />';
