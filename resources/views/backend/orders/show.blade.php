9cb431c830ec27c8247e4de35b2cabdc34aec33a
Pham Viet <vietpn@vnptepay.com>
2019-06-21 14:36:48 +0700

    update

--- resources/views/backend/orders/show.blade.php
+++ resources/views/backend/orders/show.blade.php
@@ -36,16 +36,16 @@
                 <div class="row" style="padding-left: 20px; padding-right: 20px">
                     <table class="table table-striped table-bordered table-hover">
                         <tr>
-                            <th>Tên sản phẩm</th>
                             <th>ID</th>
+                            <th>Tên sản phẩm</th>
                             <th>Ảnh</th>
                             <th>Số lượng</th>
                             <th>Giá</th>
                         </tr>
                         <?php foreach ($orderDetails as $orderDetail) { ?>
                             <tr>
-                                <td>{!! $orderDetail['product_id']['name'] !!}</td>
                                 <td>{!! $orderDetail['product_id']['id'] !!}</td>
+                                <td>{!! $orderDetail['product_id']['name'] !!}</td>
                                 <td>{!! Html::image($orderDetail['product_id']['img_path'], '',['style' => 'width:80px; height:80px']); !!}</td>
                                 <td>{!! $orderDetail->amount !!}</td>
                                 <td>{!! \App\Define\Systems::formatPrice($orderDetail['product_id']['price']) !!}</td>
